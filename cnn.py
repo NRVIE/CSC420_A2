63a6393cb001af1ba9e1c0264ce66b731d3647e5
NRVIE <62114085+NRVIE@users.noreply.github.com>
2023-10-27 08:28:17 -0400

    Try

--- cnn.py
+++ cnn.py
@@ -86,6 +86,14 @@ class DBI_CNN(nn.Module):
         acc = accuracy(out, targets)
         return {'val_acc': acc.detach(), 'val_loss': loss.detach()}
 
+    # validation epoch end
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
 
 def accuracy(outputs, labels):
     _, preds = torch.max(outputs, dim=1)
